933c78a80bb4c55a263536f9236a3f8e494a5fec
Aleix Boné <aleix.boneribo@bsc.es>
2025-12-02 14:39:43 +0100

    Fix moved package linuxPackages.perf is now perf
    Reviewed-by: Rodrigo Arias Mallo <rodrigo.arias@bsc.es>

--- m/common/base/env.nix
+++ m/common/base/env.nix
@@ -1,10 +1,10 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     vim wget git htop tmux pciutils tcpdump ripgrep nix-index nixos-option
     nix-diff ipmitool freeipmi ethtool lm_sensors cmake gnumake file tree
-    ncdu config.boot.kernelPackages.perf ldns pv
+    ncdu perf ldns pv
     # From jungle overlay
     osumb nixgen
   ];
