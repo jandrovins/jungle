22420e6ac897e41d29deb425de1c9a6925507949
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2026-01-08 15:56:56 +0100

    Remove unneeded perf package from eudy
    It is already included in the base list of packages, which is now only
    "perf" and doesn't depend on the kernel version.
    
    Reviewed-by: Aleix Boné <abonerib@bsc.es>

--- m/eudy/kernel/perf.nix
+++ m/eudy/kernel/perf.nix
@@ -1,11 +1,6 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 
 {
-  # add the perf tool
-  environment.systemPackages = with pkgs; [
-    config.boot.kernelPackages.perf
-  ];
-
   # allow non-root users to read tracing data from the kernel
   boot.kernel.sysctl."kernel.perf_event_paranoid" = -2;
   boot.kernel.sysctl."kernel.kptr_restrict" = 0;
